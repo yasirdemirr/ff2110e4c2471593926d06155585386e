6b895d4bd7718b1e496f39b8d40a46a82ddf4f92
yasirdemirr <yasir.demirr@gmail.com>
2020-12-25 14:51:16 +0300

    [DEV][yasir.demir][inital commit]

---
+++ app/src/androidTest/java/com/myd/ff2110e4c2471593926d06155585386e/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.myd.ff2110e4c2471593926d06155585386e
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.myd.ff2110e4c2471593926d06155585386e", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/myd/ff2110e4c2471593926d06155585386e/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.myd.ff2110e4c2471593926d06155585386e
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
